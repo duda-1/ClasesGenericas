d69e593a07ea9ab59842916f3b14349d0b3676c3
Alunos <eduardacristinasilva2008@gmail.com>
2024-12-06 16:21:21 -0300

    par

--- Par.cs
+++ Par.cs
@@ -8,18 +8,7 @@ namespace ClasesGenericas
 {
     public class Par<T1, T2>
     {
-        List<T1> list = new List<T1>();
-
-        public void Adicionar(T1 item1,T2 item2)
-        {
-            list.Add(item1);
-        }
-        public void Listar()
-        {
-            foreach (T1 pilha in list)
-            {
-                Console.WriteLine(pilha);
-            }
-        }
+        T1 propriedadeUm { get; set; }                      
+        T2 propriedadeDois { get; set; }
     }
 }
